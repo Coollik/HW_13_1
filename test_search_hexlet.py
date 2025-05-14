9231a99e860a2bc68cafb45380e953ae594306ec
Aleksey Kulikov <kulikov.alx@yandex.ru>
2025-05-14 19:07:46 +0400

    настроил поправил тесты

--- test_search_hexlet.py
+++ test_search_hexlet.py
@@ -1,6 +1,11 @@
 from selene import browser, be, have
 
-def test_find_hexlet(browser):
-
+def test_find_hexlet(setting_browser):
+    browser.open('https://duckduckgo.com')
     browser.element('[name="q"]').should(be.blank).type('hexlet').press_enter()
     browser.element('#r1-0 [class~="CXMyPcQ6nDv47DKFeywM"]').should(have.text('Хекслет — онлайн-школа программирования, онлайн-обучение ИТ-профессиям'))
+
+def test_find_text_hexlet(setting_browser):
+    browser.open('https://duckduckgo.com')
+    browser.element('[name="q"]').should(be.blank).type('hexlet').press_enter()
+    browser.element('#r1-0 [class~="CXMyPcQ6nDv47DKFeywM"]').should(have.text('Hexlet'))
